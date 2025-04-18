d8679386214b91d0ffdbd6115b9d562ee238b981
Chie Nakagawa <chie.design22@gmail.com>
2025-04-18 23:46:35 +0900

    App.tsx TS変換

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import { Header } from './components/Header';
+import { ArticleList } from './pages/ArticleList';
+import { ArticleDetail } from './pages/ArticleDetail';
+import './App.css';
+import { Contact } from './pages/Contact';
 
-function App() {
+const App:React.FC =() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          test
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<ArticleList />} />
+        <Route path="/posts/:id" element={<ArticleDetail />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
